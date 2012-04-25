79dbe003d4b757af83ccb77a2febf73f6b9875d0
Dongwook Lee <i015005@gmail.com>
2012-04-25 13:04:34 +0900

    Add a sample test

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('port', 3000);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -27,11 +28,15 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+app.configure('test', function(){
+ app.set('port', 3001);
+});
+
 // Routes
 
 //app.get('/', routes.index);
 require('./apps/authentication/routes')(app)
 
-app.listen(3000, function(){
+app.listen(app.settings.port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
--- test/apps/authentication-test.coffee
+++ test/apps/authentication-test.coffee
@@ -1,5 +1,16 @@
+require '../_helper'
+express  = require 'express'
+assert  = require 'assert'
+request = require 'request'
+app     = require '../../server'
 
-describe "authentication", ->
-	describe "GET /login ", ->
-		before ->
-      #...
+describe 'authentication', ->
+  describe 'GET /login', ->
+    body = null
+    before (done)->
+      request {uri: "http://localhost:#{app.settings.port}/login"}, (err, response, _body) ->
+        console.log(_body)
+        body = _body
+        done()
+    it "has user field", ->
+      assert.ok /user/.test(body)
